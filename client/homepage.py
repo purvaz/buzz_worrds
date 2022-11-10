07ce2aadf4f0173242da720858d67718671c8083
DESKTOP-QRJK9JR\Araceli Ramirez <chelaramirez@sbcglobal.net>
2022-11-09 19:31:51 -0800

    Homepage version 1

--- client/homepage.py
+++ client/homepage.py
@@ -1,2 +1,33 @@
 
 # This is the homepage
+import pygame
+
+# initialize pygame
+pygame.init()
+
+# Change the title, logo
+pygame.display.set_caption("BuzzWords")
+
+# Image.png 32 x 32 pixel size
+icon = pygame.image.load("C:/Users/Araceli Ramirez/Documents/buzz_worrds/client/venv/Bee.png")
+pygame.display.set_icon(icon)
+
+#create the screen
+screen = pygame.display.set_mode((1440,810))
+
+# create a background
+background = pygame.image.load('C:/Users/Araceli Ramirez/Documents/buzz_worrds/client/venv/Buzzwords_background_-_version_A.png')
+background_rect = background.get_rect(topleft = (0,50))
+
+
+# Game loop
+while True:
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+
+
+    pygame.display.update()
